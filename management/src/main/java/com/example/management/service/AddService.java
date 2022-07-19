b65a6effde45a1ecf19587f41dbbceb151df63cb
yangkeloff <yangkeloff@foxmail.com>
2022-07-19 16:52:29 +0800

    java init

---
+++ management/src/main/java/com/example/management/service/AddService.java
@@ -0,0 +1,12 @@
+package com.example.management.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddService {
+
+    public String add(String postJson){
+
+        return "add success";
+    }
+}
